0c0541533d1ff596936f763f0cdd44c1e3e04cc9
Josephine <kimkojosephine@gmail.com>
2026-03-22 18:36:21 +0300

    Clean up validation messages to show one error per field

--- app/models/va_profile.rb
+++ app/models/va_profile.rb
@@ -1,2 +1,21 @@
 class VaProfile < ApplicationRecord
+  VALID_NICHES = [
+    "General VA",
+    "Travel & Itinerary VA",
+    "Creative VA",
+    "Social Media VA",
+    "Lead Gen VA",
+    "Real Estate VA",
+    "Tech VA"
+  ].freeze
+
+  validates :niche, inclusion: {
+    in: VALID_NICHES,
+    message: "Please select a valid VA niche from the list"
+  }
+
+  validates :budget, numericality: {
+    greater_than_or_equal_to: 0,
+    message: "Please enter a budget of 0 or more"
+  }, allow_blank: false
 end
